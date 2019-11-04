c89c688a3f7c742b8e4ad781e9037da6958d3f81
NurtilekZ <nzakonbekov@gmail.com>
2019-11-04 22:25:18 +0600

    Update .gitignore

---
+++ Assets/Scripts/ScreenBoundaries.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ScreenBoundaries : MonoBehaviour {
+    public Camera MainCamera;
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    // Use this for initialization
+    private void Start () {
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
+    }
+
+    // Update is called once per frame
+    private void LateUpdate(){
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        var viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        transform.position = viewPos;
+    }
+}
